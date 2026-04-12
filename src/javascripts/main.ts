36971b29244e0b0d92931af2a9a86fb6cd85207d
Slimy sharky <slimy.sharky@proton.me>
2026-04-12 20:26:04 +0900

    商品画像を更新する

--- src/javascripts/main.ts
+++ src/javascripts/main.ts
@@ -1,36 +1,17 @@
-import stickySlimeMixUrl from '../assets/sticky_slime_mix.webp';
-import howToMakeStickySlime1Url from '../assets/how_to_make_sticky_slime_1.webp';
-import howToMakeStickySlime2Url from '../assets/how_to_make_sticky_slime_2.webp';
-import howToMakeStickySlime3Url from '../assets/how_to_make_sticky_slime_3.webp';
-import howToMakeStickySlime4Url from '../assets/how_to_make_sticky_slime_4.webp';
+import stickySlimeMix5lUrl from '../assets/sticky_slime_mix_5l.webp';
+import stickySlimeMix25lUrl from '../assets/sticky_slime_mix_25l.webp';
 import bucketUrl from '../assets/bucket.webp';
 import bottleUrl from '../assets/bottle.webp';
 import bakingSodaUrl from '../assets/baking_soda.webp';
 
-const stickySlimeMixImages = document.querySelectorAll<HTMLImageElement>('.sticky-slime-mix');
-stickySlimeMixImages.forEach(image => {
-  image.src = stickySlimeMixUrl;
-});
-
-const howToMakeStickySlime1Image = document.getElementById('how-to-make-sticky-slime-1') as HTMLImageElement;
-if (howToMakeStickySlime1Image !== null) {
-  howToMakeStickySlime1Image.src = howToMakeStickySlime1Url;
+const stickySlimeMix5lImages = document.getElementById('sticky-slime-mix-5l') as HTMLImageElement;
+if (stickySlimeMix5lImages !== null) {
+  stickySlimeMix5lImages.src = stickySlimeMix5lUrl;
 }
 
-
-const howToMakeStickySlime2Image = document.getElementById('how-to-make-sticky-slime-2') as HTMLImageElement;
-if (howToMakeStickySlime2Image !== null) {
-  howToMakeStickySlime2Image.src = howToMakeStickySlime2Url;
-}
-
-const howToMakeStickySlime3Image = document.getElementById('how-to-make-sticky-slime-3') as HTMLImageElement;
-if (howToMakeStickySlime3Image !== null) {
-  howToMakeStickySlime3Image.src = howToMakeStickySlime3Url;
-}
-
-const howToMakeStickySlime4Image = document.getElementById('how-to-make-sticky-slime-4') as HTMLImageElement;
-if (howToMakeStickySlime4Image !== null) {
-  howToMakeStickySlime4Image.src = howToMakeStickySlime4Url;
+const stickySlimeMix25lImages = document.getElementById('sticky-slime-mix-25l') as HTMLImageElement;
+if (stickySlimeMix25lImages !== null) {
+  stickySlimeMix25lImages.src = stickySlimeMix25lUrl;
 }
 
 const bucketImage = document.getElementById('bucket') as HTMLImageElement;
